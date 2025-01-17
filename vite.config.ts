a69635457d872f2de699f19517fe54071d354f64
mcell <minorcell6789@gmail.com>
2025-01-17 18:50:10 +0800

    feat(package): add core-js for polyfill support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+      additionalLegacyPolyfills: ['core-js/stable'],
+    })
   ],
 })
